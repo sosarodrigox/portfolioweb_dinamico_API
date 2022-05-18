9bc5239a9a8b8dace113987ba9aa0087eefed89f
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-05-18 16:12:20 -0300

    Cambie el personamodel para que se ajuste al portfolio

--- src/main/java/com/app/portfolio/controller/Controller.java
+++ src/main/java/com/app/portfolio/controller/Controller.java
@@ -2,17 +2,14 @@
 package com.app.portfolio.controller;
 import com.app.portfolio.controller.model.Persona;
 import com.app.portfolio.service.IPersonaService;
-//import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/app/portfolio/controller/model/Persona.java
+++ src/main/java/com/app/portfolio/controller/model/Persona.java
@@ -1,5 +1,4 @@
 package com.app.portfolio.controller.model;
-
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.Entity;
@@ -15,38 +14,71 @@ public class Persona {
     private Integer id;
     
     private String nombre;
-    private  String apellido;
-    private  String domicilio;
-    private  String fechaNac;
-    private  String telefono; 
-    private  String personacol; 
-    private  String correo;
-    private  String sobre_mi_bold;
-    private  String url_foto; 
-    
+    private String apellido;
+    private String posicion;
+    private String ubicacion;
+    private String nom_usuario;
+    private String img_usuario;
+    private String img_perfil;
+    private String email;
+    private String link_cv;
+    private String link_github;
+    private String link_linkedin;
+    private String titulo_slide1;
+    private String desc_slide1;
+    private String titulo_slide2;
+    private String desc_slide2;
+    private String titulo_slide3;
+    private String desc_slide3;
+    private String acerca_de_mi;
+    private String me_interesa;
+    private String hobbies;
+
     public Persona(){
     }
     
      public Persona(Integer id, 
-                    String nombre, 
+                    String nombre,
                     String apellido,
-                    String domicilio,
-                    String fechaNac,
-                    String telefono,
-                    String personacol,
-                    String correo,
-                    String sobre_mi_bold,
-                    String url_foto){
+                    String posicion,
+                    String ubicacion,
+                    String nom_usuario,
+                    String img_usuario,
+                    String img_perfil,
+                    String email,
+                    String link_cv,
+                    String link_github,
+                    String link_linkedin,
+                    String titulo_slide1,
+                    String desc_slide1,
+                    String titulo_slide2,
+                    String desc_slide2,
+                    String titulo_slide3,
+                    String desc_slide3,
+                    String acerca_de_mi,
+                    String me_interesa,
+                    String hobbies            
+             ){
          this.id = id;
          this.nombre = nombre;
          this.apellido = apellido;
-         this.domicilio = domicilio;
-         this.fechaNac = fechaNac;
-         this.telefono = telefono;
-         this.personacol = personacol;
-         this.correo = correo;
-         this.sobre_mi_bold = sobre_mi_bold;
-         this.url_foto = url_foto;
-    }
-    
+         this.posicion = posicion;
+         this.ubicacion = ubicacion;
+         this.nom_usuario = nom_usuario;
+         this.img_usuario = img_usuario;
+         this.img_perfil = img_perfil;
+         this.email = email;
+         this.link_cv = link_cv;
+         this.link_github = link_github;
+         this.link_linkedin = link_linkedin;
+         this.titulo_slide1 = titulo_slide1;
+         this.desc_slide1 = desc_slide1;
+         this.titulo_slide2 = titulo_slide2;
+         this.desc_slide2 = desc_slide2;
+         this.titulo_slide3 = titulo_slide3;
+         this.desc_slide3 = desc_slide3;
+         this.acerca_de_mi = acerca_de_mi;
+         this.me_interesa = me_interesa;
+         this.hobbies = hobbies;
+    } 
 }
