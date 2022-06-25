80afc597079fca8fa6fe9ae641ddbdf50a474e10
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-06-25 19:06:32 -0300

    JWT Authentication - CAP_1

---
+++ src/main/java/security/entity/UsuarioPrincipal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package security.entity;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class UsuarioPrincipal {
+
+}
---
+++ src/main/java/security/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package security.repository;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public interface UsuarioRepository {
+    
+}
