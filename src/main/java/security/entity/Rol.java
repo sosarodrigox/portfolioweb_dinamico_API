51b2ab148cd3f12ea93c079fd1a365fb44b7907c
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-06-25 20:58:08 -0300

    JWT Authentication - CAP_2

--- src/main/java/security/entity/Rol.java
+++ src/main/java/security/entity/Rol.java
@@ -1,14 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package security.entity;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
-public class Rol {
+import com.sun.istack.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import security.enums.RolNombre;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter //Esto se puede agregar gracias a la dependecia Lombock de springboot
+@Entity
+public class Rol {
+    
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+        @NotNull
+        @Enumerated(EnumType.STRING)
+        private RolNombre rolNombre;
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }    
 }
--- src/main/java/security/entity/Usuario.java
+++ src/main/java/security/entity/Usuario.java
@@ -1,14 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package security.entity;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
-public class Usuario {
+import com.sun.istack.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter //Esto se puede agregar gracias a la dependecia Lombock de springboot
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull
+    private String nombre;
+    @NotNull
+    @Column(unique = true)
+    private String nombreUsuario;
+    @NotNull
+    private String email;
+    @NotNull
+    private String  password;
+    //Se crean dos tablas una de usuario y otra de rol y se relacionan de la
+    //forma un usuario tiene diferentes roles.
+    @NotNull
+    @ManyToMany
+    @JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name ="usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<Rol> roles = new HashSet<>();
+
+    public Usuario() {
+    }
+
+    public Usuario(@NotNull String nombre, @NotNull String nombreUsuario, @NotNull String email, @NotNull String password) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.password = password;
+    }
 }
