33fa24d95efe39a230c42bd51f9d609a27902b74
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-06-26 11:48:11 -0300

    JWT Authentication - CAP_4

--- src/main/java/security/jwt/JwtEntryPoint.java
+++ src/main/java/security/jwt/JwtEntryPoint.java
@@ -1,14 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package security.jwt;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
-public class JwtEntryPoint {
+import java.io.IOException;
+import org.slf4j.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint{
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+    @Override
+    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException e) throws IOException, ServletException {
+        logger.error("fail en el método commnece");
+        res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No autorizado");
+        //Ver acá si no lleva s:"No autoriado" - Cap 4 3:30
+    }
 }
